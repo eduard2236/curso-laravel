ab9657d98235b9416fcf9adaf9b11ee1532469b9
eduard2236 <eduardcolmenares2236@gmail.com>
2020-06-27 05:40:28 -0430

    modificando rutas

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+ //   return view('welcome');
+//});
+
+Route::get('permiso/{nombre?}', function($nombre= 'eduard' ){
+    return $nombre;
+});//->where('nombre', '[0-9]+')->name('permiso');
